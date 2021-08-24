3d05faf7e322167a61986e1a9b636c3f78612807
Savannah Kolb <savannahnkolb@gmail.com>
2021-08-23 20:54:32 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
+
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -14,8 +26,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+}); 
+
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
+// *********** pulled this from activity 21
