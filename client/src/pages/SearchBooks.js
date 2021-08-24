9e84e6377906081e67644faa5cfbaf4cad6ab857
Savannah Kolb <savannahnkolb@gmail.com>
2021-08-23 21:58:57 -0400

    work?

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/react-hooks';
+import { SAVE_BOOK } from '../utils/mutation';
+
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
@@ -53,6 +56,7 @@ const SearchBooks = () => {
   };
 
   // create function to handle saving a book to our database
+  const [saveBook] = useMutation(saveBook);
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
