e2d614cc43ea14e6feb8723dd28d7d44b02d36fe
Savannah Kolb <savannahnkolb@gmail.com>
2021-08-23 23:10:58 -0400

    Schema info never pushed - this time?

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    bookId: ID
+    authors: [String]
+    createdAt: String
+    description: [Description]
+    title: String
+    image: String
+    link: String
+  }
+  type Description {
+    bookId: ID
+    authors: [String]
+    descriptionBody: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    savedBooks(username: String): [Book]
+    book(_id: ID!): Book
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookText: Description!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
