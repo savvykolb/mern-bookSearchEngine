1ceb33a116ba11dbef2e27181d8054fb0d926589
Savannah Kolb <savannahnkolb@gmail.com>
2021-08-26 21:28:33 -0400

    more changes - still not fixing error

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -69,9 +69,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { ...bookToSave }
+      })
 
-      if (!response.ok) {
+      if (!data.ok) {
         throw new Error('something went wrong!');
       }
 
