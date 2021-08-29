b251f9d3517891b561b0a6e85c22bc5c80d4ffd2
Savannah Kolb <savannahnkolb@gmail.com>
2021-08-28 21:38:23 -0400

    attempting to fix heroku issues

--- server/server.js
+++ server/server.js
@@ -1,38 +1,39 @@
-const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
-const path = require('path');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+const db = require("./config/connection");
 
-const {typeDefs, resolvers} = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
-const db = require('./config/connection');
-
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware, 
+  context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
-
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.json());
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+async function startServerWithMiddleware() {
+  await server.start();
+  server.applyMiddleware({ app });
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-}); 
+startServerWithMiddleware();
 
-db.once('open', () => {
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`Application listening on http://localhost:${PORT}`);
+    console.log(
+      `GraphQL now listening at http://localhost:${PORT}${server.graphqlPath}`
+    );
   });
 });
 
